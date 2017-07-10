9bb991d071bc3d696845849fc1eafa4839bdd7ce
Andy Marke <andy.marke@gmail.com>
2017-07-10 14:13:25 +0100

    iniital version

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+export KUBE_NAMESPACE=${KUBE_NAMESPACE:-${DRONE_DEPLOY_TO}}
+export ENVIRONMENT=${ENVIRONMENT:-dev}
+export KUBE_SERVER=${KUBE_SERVER_DEV}
+
+cd kd
+kd --insecure-skip-tls-verify \
+   --file deployment.yaml \
+   --file service.yaml \
+   --retries 50
